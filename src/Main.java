92546c3a1c32e5304049ef8153791fbfa956d743
DARSH <megh.darshmangal@gmail.com>
2026-04-02 12:16:41 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        Solution obj = new Solution();
+        int[] result = obj.twoSum(nums, target);
+
+        System.out.println(result[0] + " " + result[1]);
+    }
+}
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+
+            if (map.containsKey(diff)) {
+                return new int[]{map.get(diff), i};
+            }
+
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
